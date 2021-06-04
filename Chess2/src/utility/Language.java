b4c86a3f8029c02057f484b00c5f9a8e8852777e
victor dewitte <victor.dewitte@student.hogent.be>
2021-06-04 22:27:50 +0200

    Naming classes i might need

---
+++ Chess2/src/utility/Language.java
@@ -0,0 +1,55 @@
+package utility;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+
+
+
+public class Language {
+    private static ResourceBundle msg;
+   // private DomeinController dc;
+    public Language(int t) {
+        kiesTaal(t);
+
+    }
+
+    private void kiesTaal(int keuze) {
+        //Scanner scan = new Scanner(System.in);
+        String lang; 
+        String country;
+       // int keuze = 0;
+        
+//        do {
+//        System.out.print("1 = Nederlands\n2 = English\n3 = Français"
+//                + "\nKies taal/Choose language/Choisissez la langue: ");
+//        keuze = scan.nextInt();
+//        }while(keuze < 1 || keuze > 3);
+        if(keuze == 1){
+            lang = "nl";
+            country = "BE";
+        }
+        else if(keuze == 2) {
+            lang = "en";
+            country = "UK";
+        }
+        else if(keuze == 3) {
+            lang = "fr";
+            country = "FR";
+        }
+        else {
+            lang = "nl";
+            country = "BE";
+        }
+        
+        Locale locale = new Locale(lang, country);
+        msg = ResourceBundle.getBundle("resources.lang",locale);
+        
+        
+        
+    }
+    
+    public static String getString(String bericht) {
+        return msg.getString(bericht);
+    }
+}
